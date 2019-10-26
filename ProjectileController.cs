f163ceacd40f9c09200a8b3c1a21213b7c1456d5
Chehui Chou <deadshot465@users.noreply.github.com>
2019-10-26 22:27:34 +0900

    Add particle controller.

---
+++ ProjectileController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Xna.Framework;
+using Nez;
+
+namespace TeamProject3
+{
+    public class ProjectileController : Component, IUpdatable
+    {
+        ProjectileMover _mover;
+
+        public Vector2 Velocity;
+
+        public ProjectileController(Vector2 velocity) => Velocity = velocity;
+
+        void IUpdatable.Update()
+        {
+            if (_mover.Move(Velocity * Time.DeltaTime) || Entity.Position.X > 1200)
+            {
+                Entity.Destroy();
+            }
+        }
+
+        public override void OnAddedToEntity()
+        {
+            base.OnAddedToEntity();
+
+            _mover = Entity.GetComponent<ProjectileMover>();
+        }
+    }
+}
--- Scene/GameScene.cs
+++ Scene/GameScene.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using Nez;
+using Nez.Particles;
 using Nez.Sprites;
 using Nez.Textures;
 using Microsoft.Xna.Framework;
@@ -17,10 +18,14 @@ namespace TeamProject3.Scene
         private List<Entity> _playerProjectiles = new List<Entity>();
         private Entity _playerGuardProjectile;
         private SpriteAnimator _playerAnimator;
-        private VirtualButton _playerAttackButtonZ = new VirtualButton();
-        private VirtualButton _playerAttackButtonX = new VirtualButton();
-        private VirtualButton _playerAttackButtonC = new VirtualButton();
-        private VirtualButton _playerAttackButtonV = new VirtualButton();
+        private const float _animationFramerate = 20.0f;
+
+        private enum FireInput
+        {
+            Z, X, C, V
+        }
+        private Dictionary<FireInput, VirtualButton> _playerAttackButtons
+            = new Dictionary<FireInput, VirtualButton>();
 
         private bool _guarding = false;
 
@@ -31,23 +36,7 @@ namespace TeamProject3.Scene
         public override void Initialize()
         {
             base.Initialize();
-
-            var playerTexture = Content.Load<Texture2D>("player_revised");
-            var playerAtlas = Sprite.SpritesFromAtlas(playerTexture, 128, 128);
-            _playerCharacterEntity = CreateEntity("sample-player");
-            _playerAnimator = _playerCharacterEntity.AddComponent(new SpriteAnimator());
-            _playerAnimator.AddAnimation("running",
-                new SpriteAnimation(playerAtlas.ToArray()[0..3], 30.0f));
-            _playerAnimator.AddAnimation("guard",
-                new SpriteAnimation(playerAtlas.ToArray()[3..6], 30.0f));
-            _playerAnimator.AddAnimation("parry",
-                new SpriteAnimation(playerAtlas.ToArray()[6..9], 30.0f));
-            _playerAnimator.AddAnimation("damage",
-                new SpriteAnimation(playerAtlas.ToArray()[9..12], 30.0f));
-            _playerAnimator.AddAnimation("prepare",
-                new SpriteAnimation(playerAtlas.ToArray()[12..15], 30.0f));
-
-            _playerCharacterEntity.Position = new Vector2(250, 250);
+            SetupCharacterAnimation();
 
             _playerGuardProjectile = CreateEntity("guard-matrix");
             _playerGuardProjectile.SetParent(_playerCharacterEntity);
@@ -55,10 +44,35 @@ namespace TeamProject3.Scene
                 ParticleSystem.CreateEmitter(ParticleSystem.ParticleType.Electrons));
             _playerGuardProjectile.SetEnabled(false);
 
-            _playerAttackButtonZ.AddKeyboardKey(Keys.Z);
-            _playerAttackButtonX.AddKeyboardKey(Keys.X);
-            _playerAttackButtonC.AddKeyboardKey(Keys.C);
-            _playerAttackButtonV.AddKeyboardKey(Keys.V);
+            _playerAttackButtons.Add(FireInput.Z, new VirtualButton());
+            _playerAttackButtons.Add(FireInput.X, new VirtualButton());
+            _playerAttackButtons.Add(FireInput.C, new VirtualButton());
+            _playerAttackButtons.Add(FireInput.V, new VirtualButton());
+
+            _playerAttackButtons[FireInput.Z].AddKeyboardKey(Keys.Z);
+            _playerAttackButtons[FireInput.X].AddKeyboardKey(Keys.X);
+            _playerAttackButtons[FireInput.C].AddKeyboardKey(Keys.C);
+            _playerAttackButtons[FireInput.V].AddKeyboardKey(Keys.V);
+        }
+
+        private void SetupCharacterAnimation()
+        {
+            var playerTexture = Content.Load<Texture2D>("player_revised");
+            var playerAtlas = Sprite.SpritesFromAtlas(playerTexture, 128, 128);
+            _playerCharacterEntity = CreateEntity("sample-player");
+            _playerAnimator = _playerCharacterEntity.AddComponent(new SpriteAnimator());
+            _playerAnimator.AddAnimation("running",
+                new SpriteAnimation(playerAtlas.ToArray()[0..3], _animationFramerate));
+            _playerAnimator.AddAnimation("guard",
+                new SpriteAnimation(playerAtlas.ToArray()[3..6], _animationFramerate));
+            _playerAnimator.AddAnimation("parry",
+                new SpriteAnimation(playerAtlas.ToArray()[6..9], _animationFramerate));
+            _playerAnimator.AddAnimation("damage",
+                new SpriteAnimation(playerAtlas.ToArray()[9..12], _animationFramerate));
+            _playerAnimator.AddAnimation("prepare",
+                new SpriteAnimation(playerAtlas.ToArray()[12..15], _animationFramerate));
+
+            _playerCharacterEntity.Position = new Vector2(250, 250);
 
             _playerAnimator.OnAnimationCompletedEvent += animationName =>
             {
@@ -83,58 +97,60 @@ namespace TeamProject3.Scene
             base.Update();
             ClearColor = Color.Black;
 
-            if (_playerAttackButtonZ.IsPressed)
+            var pressedButtons = _playerAttackButtons
+                .Values
+                .Where(button => button.IsPressed);
+
+            if (pressedButtons.ToList().Count > 0)
             {
-                var entity = CreateEntity("projectile");
-                entity.SetParent(_playerCharacterEntity);
-                entity.AddComponent(ParticleSystem.CreateEmitter(ParticleSystem.ParticleType.Fire));
-                _playerProjectiles.Add(entity);
-                _playerAnimator.Play("parry", SpriteAnimator.LoopMode.Once);
+                foreach (var button in pressedButtons)
+                {
+                    var entity = CreateEntity("projectile");
+                    entity.Position = _playerCharacterEntity.Position;
+                    entity.AddComponent(SelectEmitterType(button));
+                    entity.AddComponent(new ProjectileMover());
+                    entity.AddComponent(new ProjectileController(new Vector2(175.0f, 0.0f)));
+
+                    var collider = entity.AddComponent<CircleCollider>();
+                    Flags.SetFlagExclusive(ref collider.CollidesWithLayers, 0);
+                    Flags.SetFlagExclusive(ref collider.PhysicsLayer, 1);
+
+                    //_playerProjectiles.Add(entity);
+                    _playerAnimator.Play("parry", SpriteAnimator.LoopMode.Once);
+                }
             }
 
-            if (_playerAttackButtonX.IsPressed)
-            {
-                var entity = CreateEntity("projectile");
-                entity.SetParent(_playerCharacterEntity);
-                entity.AddComponent(ParticleSystem.CreateEmitter(ParticleSystem.ParticleType.Comet));
-                _playerProjectiles.Add(entity);
-                _playerAnimator.Play("parry", SpriteAnimator.LoopMode.Once);
-            }
-
-            if (_playerAttackButtonC.IsPressed)
-            {
-                var entity = CreateEntity("projectile");
-                entity.SetParent(_playerCharacterEntity);
-                entity.AddComponent(ParticleSystem.CreateEmitter(ParticleSystem.ParticleType.BlueFlame));
-                _playerProjectiles.Add(entity);
-                _playerAnimator.Play("parry", SpriteAnimator.LoopMode.Once);
-            }
-
-            if (_playerAttackButtonV.IsDown && !_guarding)
+            if (_playerAttackButtons[FireInput.V].IsDown && !_guarding)
             {
                 _playerGuardProjectile.SetEnabled(true);
                 _playerAnimator.Play("guard", SpriteAnimator.LoopMode.Loop);
                 _guarding = true;
             }
-            else if (_playerAttackButtonV.IsReleased && _guarding)
+            else if (_playerAttackButtons[FireInput.V].IsReleased && _guarding)
             {
                 _playerGuardProjectile.SetEnabled(false);
                 _playerAnimator.Play("running", SpriteAnimator.LoopMode.Loop);
                 _guarding = false;
             }
 
-            _playerProjectiles.ForEach(entity =>
-            {
-                entity.Position = new Vector2(entity.Position.X + 10, entity.Position.Y);
-            });
-
             _playerProjectiles.Where(entity => entity.Position.X > 1200).ToList()
                 .ForEach(entity =>
                 {
                     entity.RemoveAllComponents();
+                    //entity.Destroy();
                 });
-
-            _playerProjectiles.RemoveAll(entity => entity.Position.X > 1200);
         }
+
+        private ParticleEmitter SelectEmitterType(VirtualButton button) =>
+            button switch
+            {
+                var x when x == _playerAttackButtons[FireInput.Z] =>
+                ParticleSystem.CreateEmitter(ParticleSystem.ParticleType.Fire),
+                var x when x == _playerAttackButtons[FireInput.X] =>
+                ParticleSystem.CreateEmitter(ParticleSystem.ParticleType.Comet),
+                var x when x == _playerAttackButtons[FireInput.C] =>
+                ParticleSystem.CreateEmitter(ParticleSystem.ParticleType.BlueFlame),
+                _ => ParticleSystem.CreateEmitter(ParticleSystem.ParticleType.WinnerStars)
+            };
     }
 }
