13d9c9ad626a65601e092f8d3fab7c02e669abaf
Chehui Chou <deadshot465@users.noreply.github.com>
2019-11-05 15:46:29 +0900

    Add player class.

---
+++ Player.cs
@@ -0,0 +1,169 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Nez;
+using Nez.Sprites;
+using Nez.Textures;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace TeamProject3
+{
+    public class Player : Component, IUpdatable
+    {
+        private SubpixelVector2 _subpixelVector = new SubpixelVector2();
+        private Vector2 _startPosition = Vector2.Zero;
+        private SpriteAnimator _spriteAnimator;
+        private Mover _mover;
+        private float _animationFramerate = 0.0f;
+        private BoxCollider _collider;
+        private float _speed = 0.0f;
+        private const float _gravity = 9.8f;
+
+        private enum Input
+        {
+            Attack, Flee
+        }
+
+        private enum Movement
+        {
+            Move, Jump
+        }
+
+        private Dictionary<Input, VirtualButton> _inputKeyMappings =
+            new Dictionary<Input, VirtualButton>();
+
+        private Dictionary<Input, Tuple<Keys, Buttons>> _inputKeys =
+            new Dictionary<Input, Tuple<Keys, Buttons>>();
+
+        private Dictionary<Movement, VirtualIntegerAxis> _inputMovementMappings =
+            new Dictionary<Movement, VirtualIntegerAxis>();
+
+        private Dictionary<Movement, Tuple<Keys, Keys>> _inputMovements =
+            new Dictionary<Movement, Tuple<Keys, Keys>>();
+
+        public Player(float speed, Vector2 startPosition, float animationFramerate)
+        {
+            _speed = speed;
+            _startPosition = startPosition;
+            _animationFramerate = animationFramerate;
+
+            _inputKeys.Add(Input.Attack, new Tuple<Keys, Buttons>(Keys.Z, Buttons.A));
+            _inputKeys.Add(Input.Flee, new Tuple<Keys, Buttons>(Keys.C, Buttons.X));
+
+            _inputMovements.Add(Movement.Move, new Tuple<Keys, Keys>(Keys.Left, Keys.Right));
+            _inputMovements.Add(Movement.Jump, new Tuple<Keys, Keys>(Keys.Up, Keys.Down));
+        }
+
+        public override void Initialize()
+        {
+            base.Initialize();
+        }
+
+        public override void OnAddedToEntity()
+        {
+            base.OnAddedToEntity();
+
+            var texture = Entity.Scene.Content.Load<Texture2D>("player_revised");
+            var spriteAtlas = Sprite.SpritesFromAtlas(texture, 128, 128);
+            _spriteAnimator = Entity.AddComponent<SpriteAnimator>();
+            _spriteAnimator.AddAnimation("running",
+                new SpriteAnimation(spriteAtlas.ToArray()[0..3], _animationFramerate));
+            _spriteAnimator.AddAnimation("guard",
+                new SpriteAnimation(spriteAtlas.ToArray()[3..6], _animationFramerate));
+            _spriteAnimator.AddAnimation("parry",
+                new SpriteAnimation(spriteAtlas.ToArray()[6..9], _animationFramerate));
+            _spriteAnimator.AddAnimation("damage",
+                new SpriteAnimation(spriteAtlas.ToArray()[9..12], _animationFramerate));
+            _spriteAnimator.AddAnimation("prepare",
+                new SpriteAnimation(spriteAtlas.ToArray()[12..15], _animationFramerate));
+
+            _collider = Entity.AddComponent<BoxCollider>();
+            Flags.SetFlagExclusive(ref _collider.CollidesWithLayers, 0);
+            Flags.SetFlagExclusive(ref _collider.PhysicsLayer, 1);
+
+            _mover = Entity.AddComponent<Mover>();
+
+            Entity.Position = _startPosition;
+
+            _spriteAnimator.OnAnimationCompletedEvent += animationName =>
+            {
+                _spriteAnimator.Play("running", SpriteAnimator.LoopMode.Loop);
+            };
+
+            SetupKeyboardInputs();
+
+            _spriteAnimator.Play("running", SpriteAnimator.LoopMode.Loop);
+        }
+
+        public override void OnRemovedFromEntity()
+        {
+            base.OnRemovedFromEntity();
+
+            _inputKeyMappings.ToList().ForEach(mapping =>
+            {
+                mapping.Value.Deregister();
+            });
+        }
+
+        void IUpdatable.Update()
+        {
+            var moveDirection = new Vector2(_inputMovementMappings[Movement.Move].Value,
+                _inputMovementMappings[Movement.Jump].Value);
+            if (moveDirection.Y > 0) moveDirection.Y = 0;
+
+            //moveDirection.Y += _gravity;
+
+            if (moveDirection != Vector2.Zero)
+            {
+                var movement = moveDirection * _speed * Time.DeltaTime;
+
+                _mover.CalculateMovement(ref movement, out var result);
+                _subpixelVector.Update(ref movement);
+                _mover.ApplyMovement(movement);
+            }
+
+        }
+
+        private void SetupKeyboardInputs()
+        {
+            _inputKeyMappings.Add(Input.Attack, new VirtualButton());
+            _inputKeyMappings.Add(Input.Flee, new VirtualButton());
+            _inputMovementMappings.Add(Movement.Move, new VirtualIntegerAxis());
+            _inputMovementMappings.Add(Movement.Jump, new VirtualIntegerAxis());
+
+            _inputKeyMappings[Input.Attack].Nodes
+                .Add(new VirtualButton.KeyboardKey(_inputKeys[Input.Attack].Item1));
+            _inputKeyMappings[Input.Attack].Nodes
+                .Add(new VirtualButton.GamePadButton(0, _inputKeys[Input.Attack].Item2));
+
+            _inputKeyMappings[Input.Flee].Nodes
+                .Add(new VirtualButton.KeyboardKey(_inputKeys[Input.Flee].Item1));
+            _inputKeyMappings[Input.Flee].Nodes
+                .Add(new VirtualButton.GamePadButton(0, _inputKeys[Input.Flee].Item2));
+
+            _inputMovementMappings[Movement.Move].Nodes
+                .Add(new VirtualAxis.GamePadDpadLeftRight());
+            _inputMovementMappings[Movement.Move].Nodes
+                .Add(new VirtualAxis.GamePadLeftStickX());
+
+            _inputMovementMappings[Movement.Jump].Nodes
+                .Add(new VirtualAxis.GamePadDpadUpDown());
+            _inputMovementMappings[Movement.Jump].Nodes
+                .Add(new VirtualAxis.GamePadLeftStickY());
+
+            AddInputAxis(Movement.Move);
+            AddInputAxis(Movement.Jump);      
+        }
+
+        private void AddInputAxis(Movement movement)
+        {
+            _inputMovementMappings[movement].Nodes
+                .Add(new VirtualAxis
+                .KeyboardKeys(VirtualInput.OverlapBehavior.TakeNewer,
+                _inputMovements[movement].Item1,
+                _inputMovements[movement].Item2));
+        }
+    }
+}
--- Scene/GameScene.cs
+++ Scene/GameScene.cs
@@ -1,35 +1,15 @@
 ﻿using Microsoft.Xna.Framework;
-using Microsoft.Xna.Framework.Graphics;
-using Microsoft.Xna.Framework.Input;
 using Nez;
-using Nez.Particles;
-using Nez.Sprites;
-using Nez.Textures;
-using System.Collections.Generic;
-using System.Linq;
 
 namespace TeamProject3.Scene
 {
     public class GameScene : Nez.Scene
     {
         private Entity _playerCharacterEntity;
-        private List<Entity> _playerProjectiles = new List<Entity>();
-        private Entity _playerGuardProjectile;
-        private SpriteAnimator _playerAnimator;
         private Entity _bossEntity;
         private const float _animationFramerate = 20.0f;
         private const float _projectileVelocity = 350.0f;
-
-        private enum FireInput
-        {
-            Z, X, C, V
-        }
-        private Dictionary<FireInput, VirtualButton> _playerAttackButtons
-            = new Dictionary<FireInput, VirtualButton>();
-
-        private VirtualButton _slowMotionSwitch = new VirtualButton();
-
-        private bool _guarding = false;
+        private Vector2 _startPosition = new Vector2(250.0f);
 
         public GameScene()
         {
@@ -39,63 +19,21 @@ namespace TeamProject3.Scene
         public override void Initialize()
         {
             base.Initialize();
-            SetupCharacterAnimation();
 
-            _playerGuardProjectile = CreateEntity("guard-matrix");
-            _playerGuardProjectile.SetParent(_playerCharacterEntity);
-            _playerGuardProjectile.AddComponent(
-                ParticleSystem.CreateEmitter(ParticleSystem.ParticleType.Electrons));
-            _playerGuardProjectile.SetEnabled(false);
+            _playerCharacterEntity = CreateEntity("player-entity");
+            _playerCharacterEntity
+                .AddComponent(new Player(200.0f, _startPosition, _animationFramerate));
 
             _bossEntity = CreateEntity("boss-entity");
-            _bossEntity.AddComponent(new Boss(new Vector2(_playerCharacterEntity.Position.X + 400,
-                _playerCharacterEntity.Position.Y), BossSettings.ImportBossSettings()));
-
-            _playerAttackButtons.Add(FireInput.Z, new VirtualButton());
-            _playerAttackButtons.Add(FireInput.X, new VirtualButton());
-            _playerAttackButtons.Add(FireInput.C, new VirtualButton());
-            _playerAttackButtons.Add(FireInput.V, new VirtualButton());
-
-            _slowMotionSwitch.Nodes.Add(new VirtualButton.KeyboardKey(Keys.A));
-
-            _playerAttackButtons[FireInput.Z].AddKeyboardKey(Keys.Z);
-            _playerAttackButtons[FireInput.X].AddKeyboardKey(Keys.X);
-            _playerAttackButtons[FireInput.C].AddKeyboardKey(Keys.C);
-            _playerAttackButtons[FireInput.V].AddKeyboardKey(Keys.V);
+            _bossEntity.AddComponent(new Boss(new Vector2(_startPosition.X + 400,
+                _startPosition.Y), BossSettings.ImportBossSettings()));
 
             BossSettings.ExportBossSettings();
-        }
-
-        private void SetupCharacterAnimation()
-        {
-            var playerTexture = Content.Load<Texture2D>("player_revised");
-            var playerAtlas = Sprite.SpritesFromAtlas(playerTexture, 128, 128);
-            _playerCharacterEntity = CreateEntity("sample-player");
-            _playerAnimator = _playerCharacterEntity.AddComponent(new SpriteAnimator());
-            _playerAnimator.AddAnimation("running",
-                new SpriteAnimation(playerAtlas.ToArray()[0..3], _animationFramerate));
-            _playerAnimator.AddAnimation("guard",
-                new SpriteAnimation(playerAtlas.ToArray()[3..6], _animationFramerate));
-            _playerAnimator.AddAnimation("parry",
-                new SpriteAnimation(playerAtlas.ToArray()[6..9], _animationFramerate));
-            _playerAnimator.AddAnimation("damage",
-                new SpriteAnimation(playerAtlas.ToArray()[9..12], _animationFramerate));
-            _playerAnimator.AddAnimation("prepare",
-                new SpriteAnimation(playerAtlas.ToArray()[12..15], _animationFramerate));
-
-            _playerCharacterEntity.Position = new Vector2(250, 250);
-
-            _playerAnimator.OnAnimationCompletedEvent += animationName =>
-            {
-                _playerAnimator.Play("running", SpriteAnimator.LoopMode.Loop);
-            };
         }
 
         public override void OnStart()
         {
             base.OnStart();
-
-            _playerAnimator.Play("running", SpriteAnimator.LoopMode.Loop);
         }
 
         public override void Unload()
@@ -108,69 +46,6 @@ namespace TeamProject3.Scene
             base.Update();
             ClearColor = Color.Black;
 
-            var pressedButtons = _playerAttackButtons
-                .Values
-                .Where(button => button.IsPressed);
-
-            if (pressedButtons.ToList().Count > 0)
-            {
-                foreach (var button in pressedButtons)
-                {
-                    var entity = CreateEntity("projectile");
-                    entity.Position = _playerCharacterEntity.Position;
-                    entity.AddComponent(SelectEmitterType(button));
-                    entity.AddComponent(new ProjectileMover());
-                    entity.AddComponent(
-                        new ProjectileController(new Vector2(_projectileVelocity, 0.0f)));
-
-                    var collider = entity.AddComponent<CircleCollider>();
-                    Flags.SetFlagExclusive(ref collider.CollidesWithLayers, 0);
-                    Flags.SetFlagExclusive(ref collider.PhysicsLayer, 1);
-
-                    //_playerProjectiles.Add(entity);
-                    _playerAnimator.Play("parry", SpriteAnimator.LoopMode.Once);
-                }
-            }
-
-            if (_playerAttackButtons[FireInput.V].IsDown && !_guarding)
-            {
-                _playerGuardProjectile.SetEnabled(true);
-                _playerAnimator.Play("guard", SpriteAnimator.LoopMode.Loop);
-                _guarding = true;
-            }
-            else if (_playerAttackButtons[FireInput.V].IsReleased && _guarding)
-            {
-                _playerGuardProjectile.SetEnabled(false);
-                _playerAnimator.Play("running", SpriteAnimator.LoopMode.Loop);
-                _guarding = false;
-            }
-
-            if (_slowMotionSwitch.IsPressed)
-            {
-                _playerAnimator.Speed /= 4;
-                Core.Schedule(2.0f, timer =>
-                {
-                    _playerAnimator.Speed = 1;
-                });
-            }
-
-            _playerProjectiles.Where(entity => entity.Position.X > 1200).ToList()
-                .ForEach(entity =>
-                {
-                    entity.RemoveAllComponents();
-                    //entity.Destroy();
-                });
         }
-
-        private ParticleEmitter SelectEmitterType(VirtualButton button) =>
-            button switch
-            {
-                var x when x == _playerAttackButtons[FireInput.Z] =>
-                ParticleSystem.CreateEmitter(ParticleSystem.ParticleType.Comet),
-                var x when x == _playerAttackButtons[FireInput.X] =>
-                ParticleSystem.CreateEmitter(ParticleSystem.ParticleType.BlueGalaxy),
-                var x when x == _playerAttackButtons[FireInput.C] =>
-                ParticleSystem.CreateEmitter(ParticleSystem.ParticleType.IntoTheBlue)
-            };
     }
 }
